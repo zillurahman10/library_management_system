c41dd79edc3f6fa5fcb2a71d16244e7bdb8d6260
zillurahman10 <zillur.hero40@gmail.com>
2024-06-13 21:15:54 +0600

    migration complte

--- books/models.py
+++ books/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.FileField(upload_to = 'books/uploads')
+    borrowing_price = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+    
+
+class Review(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"Commented by {self.user.username} on {self.book.title}"
